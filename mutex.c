a462311abe4c8a2e7d49d19507ab103a02f86cb7
Al Fachri Sagianto <163378556+23Expt@users.noreply.github.com>
2024-06-14 19:26:14 +0700

    Add files via upload

---
+++ mutex.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define LOCK_FILE "/tmp/my_mutex.lock"
+
+void acquire_lock() {
+    while (access(LOCK_FILE, F_OK) != 0) {
+        sleep(0.1);
+    }
+}
+void release_lock() {
+    unlink(LOCK_FILE);
+}
+void critical_section() {
+    printf("Critical section entered by process %d\n", getpid());
+    sleep(3); 
+    printf("Critical section completed by process %d\n", getpid());
+}
+
+int main() {
+    acquire_lock();
+    critical_section();
+    release_lock();
+
+    return 0;
+}
